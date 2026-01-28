78b19d60522200f45debd03c6dc3667da99f8f0d
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-01-28 19:32:56 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "fraction.h"
+using namespace std;
+
+int main() {
+    Fraction f1(1, 2); 
+    Fraction f2(3, 4); 
+
+    cout << "Fraction 1: " << f1.getNum() << "/" << f1.getDen() << endl;
+    cout << "Fraction 2: " << f2.getNum() << "/" << f2.getDen() << endl;
+
+    Fraction result;
+    result.somme(f1, f2);
+
+    cout << "Sum: " << result.getNum() << "/" << result.getDen() << endl;
+
+    result.simplif();
+    cout << "Simplified sum: " << result.getNum() << "/" << result.getDen() << endl;
+
+    return 0;
+}
