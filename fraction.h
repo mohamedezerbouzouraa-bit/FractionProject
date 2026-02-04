8894af9bceca4e4d2381f839bb386bbb2c4c2365
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-04 22:25:18 +0100

    Les methodes de la classe Fraction

--- fraction.h
+++ fraction.h
@@ -4,24 +4,19 @@
 
 #ifndef FRACTION_H
 #define FRACTION_H
-
 #include <iostream>
 using namespace std;
-
 class Fraction {
 private:
     int num, den;
-
     int gcd(int a, int b); // Fonction pour simplifier la fraction
 
 public:
     Fraction();
     Fraction(int n, int d);
-
     void setFraction(int n, int d);
     int getNum();
     int getDen();
-
     void simplif();
     void somme(const Fraction& f1, const Fraction& f2);
 };
